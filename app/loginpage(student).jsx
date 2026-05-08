5897533094a9e16b99c7250301910548d5133f81
Thayaparan Gopan <thayaparangopan21@gmail.com>
2026-05-08 18:09:47 +0530

    Update loginpage(student).jsx

--- app/loginpage(student).jsx
+++ app/loginpage(student).jsx
@@ -49,7 +49,7 @@ export default function LoginPage() {
         </View>
         <View style={styles.logoContainer}>
           <Image
-            source={require("../../assets/images/logo.png")}
+            source={require("../src/assets/images/logo.png")}
             style={styles.logoImage}
             resizeMode="contain"
           />
