98650d4e886be84e0f5b0a0d9c46f116ac86bc71
Asvinitha Thevaraja <asvinithevan@gmail.com>
2026-05-10 17:26:25 +0530

    Update coursedetails.jsx

--- app/coursedetails.jsx
+++ app/coursedetails.jsx
@@ -36,7 +36,6 @@ const COURSES = [
   { id: '6', title: 'Foundations of Web Technologies', tags: 'HTML • CSS • Client-Side', code: 'IS1106', type: 'Compulsory', credits: '2 Credits', image: require('../src/assets/images/WB.jpeg'), color: '#FFE082' },
   { id: '7', title: 'Personal Productivity with IT', tags: 'Tools • Documentation • Efficiency', code: 'IS1107', type: 'Compulsory', credits: '1 Credit', image: require('../src/assets/images/PPIT.jpeg'), color: '#F8BBD0' },
 
-  
   { id: '8', title: 'Fundamentals of Mathematics', tags: 'Algebra • Logic • Matrices', code: 'IS1108', type: 'Compulsory', credits: '2 Credits', image: require('../../assets/images/FOM.jpeg'), color: '#DCEDC8' },
   { id: '9', title: 'Statistics & Probability Theory', tags: 'Data • Inference • Analysis', code: 'IS1109', type: 'Compulsory', credits: '2 Credits', image: require('../../assets/images/Stat.png'), color: '#B2EBF2' },
   { id: '10', title: 'Communication Skills I', tags: 'Written • Verbal • Professional', code: 'IS1110', type: 'Compulsory (Non-GPA)', credits: '2 Credits', image: require('../../assets/images/comskills1.jpeg'), color: '#E1BEE7' },
