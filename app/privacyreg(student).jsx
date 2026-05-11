e841f1dff93f479bcbf898d6fa17842e057d1e06
Thayaparan Gopan <thayaparangopan21@gmail.com>
2026-05-11 18:34:28 +0530

    Update privacyreg(student).jsx

--- app/privacyreg(student).jsx
+++ app/privacyreg(student).jsx
@@ -80,7 +80,7 @@ export default function PrivacyScreen() {
 
       {/* 2. SUB-HEADER */}
       <View style={styles.subHeader}>
-        <TouchableOpacity onPress={() => router.replace('/coursedetails')} style={styles.backButton}>
+        <TouchableOpacity onPress={() => router.replace('/register(student)')} style={styles.backButton}>
           <Ionicons name="arrow-back" size={22} color="#1A73E8" />
           <Text style={styles.subHeaderTitle}>Privacy & Data Policy</Text>
         </TouchableOpacity>
