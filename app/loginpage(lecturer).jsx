368547919175a3c04a724d931ac88b74edeb6814
Thayaparan Gopan <thayaparangopan21@gmail.com>
2026-05-16 12:19:45 +0530

    Update loginpage(lecturer).jsx

--- app/loginpage(lecturer).jsx
+++ app/loginpage(lecturer).jsx
@@ -144,7 +144,7 @@ export default function LoginPage() {
             {/* Footer */}
             <View style={styles.footer}>
               <Text style={styles.footerText}>Don't have an account?</Text>
-              <TouchableOpacity onPress={() => router.push("/register(student)")}>
+              <TouchableOpacity onPress={() => router.push("/register(lecturer)")}>
                 <Text style={styles.signUpText}>Create an account</Text>
               </TouchableOpacity>
             </View>
