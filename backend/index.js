e69d9e9b50fa317fff5497a50723abfb77248142
Yukkeshan Koneswaran <ykshan2003@gmail.com>
2026-06-04 17:30:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -56,3 +56,13 @@ const model = genAI.getGenerativeModel({
     responseMimeType: "application/json",
   }
 });
+
+const transporter = nodemailer.createTransport({
+   service: 'gmail',
+   auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+   }
+});
+
+
