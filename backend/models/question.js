269afe54b01f931ef91695d5c78abf3c1a41d0c8
immathusa <mathuthev6@gmail.com>
2026-06-04 18:07:07 +0530

    Update question.js

--- backend/models/question.js
+++ backend/models/question.js
@@ -2,3 +2,8 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const question = sequelize.define('question', {
+    id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
