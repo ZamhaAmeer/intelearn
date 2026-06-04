fa18e28cf5eee603269227debadf18fc6386589b
Thayaparan Gopan <thayaparangopan21@gmail.com>
2026-06-04 19:29:03 +0530

    Update addnewcourse.jsx

--- app/addnewcourse.jsx
+++ app/addnewcourse.jsx
@@ -360,7 +360,7 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 30,
     borderRadius: 30,
-    marginTop: -15, // Pop-out effect
+    marginTop: -15, 
   },
   primaryButtonContent: {
     flexDirection: 'row',
