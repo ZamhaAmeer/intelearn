af7181c98ba326a8d7d83b0cbac6e09988e7ac90
immathusa <mathuthev6@gmail.com>
2026-06-04 18:13:19 +0530

    Update quiz.js

--- backend/models/quiz.js
+++ backend/models/quiz.js
@@ -2,3 +2,8 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const quiz = sequelize.define('quiz', {
+    id: {
+        type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
