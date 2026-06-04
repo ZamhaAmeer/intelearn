d07ddf6ba0a33f3bb8ffeb605519203063f9a93f
immathusa <mathuthev6@gmail.com>
2026-06-04 18:03:46 +0530

    Update enrollment.js

--- backend/models/enrollment.js
+++ backend/models/enrollment.js
@@ -1,2 +1,9 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+
+const enrollment = sequelize.define('enrollment', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
