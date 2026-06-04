275e7ee53df028c8f5c6ea6c35d34558ab5ab53e
ZamhaAmeer <zamhaameer855194@gmail.com>
2026-06-04 19:53:11 +0530

    Update coursedetails.jsx

--- app/coursedetails.jsx
+++ app/coursedetails.jsx
@@ -59,3 +59,5 @@ const COURSES = [
     // --- SEMESTER III ---
   { id: '26', title: 'Object Oriented Analysis & Design', tags: 'UML Modeling • Design Patterns', code: 'IS3101', type: 'Compulsory', credits: '2 Credits', image: require('../src/assets/images/OOAD.jpeg'), color: '#FFECB3' },
   { id: '27', title: 'Data Structures & Algorithms', tags: 'Trees • Graphs • Sorting', code: 'IS3102', type: 'Compulsory', credits: '2 Credits', image: require('../src/assets/images/DSA.jpeg'), color: '#E1F5FE' },
+  { id: '28', title: 'IT Governance', tags: 'COBIT • Compliance • Frameworks', code: 'IS3103', type: 'Compulsory', credits: '2 Credits', image: require('../src/assets/images/ITG.jpeg'), color: '#E8F5E9' },
+  
