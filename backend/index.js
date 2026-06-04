7d5eeb01cecb990636ac3262ee725e518897ebc7
Yukkeshan Koneswaran <ykshan2003@gmail.com>
2026-06-04 17:52:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -162,3 +162,7 @@ app.post('/register', async (req, res) => {
 // ------------------------------------
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ error: 'Missing email or password' });
+  }
