97f01898d3da7b4a63c8afee80f38c4995ef0418
Yukkeshan Koneswaran <ykshan2003@gmail.com>
2026-06-04 17:35:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -71,3 +71,8 @@ const transporter = nodemailer.createTransport({
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Access denied' });
+  }
+
